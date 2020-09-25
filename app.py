8a95c36cf2293724c986ffb1be8cd52acb0ec59c
Lyle Irvine <63163556+Caesar-Sec@users.noreply.github.com>
2020-09-25 15:45:36 +0100

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,10 @@ from flask import Flask, render_template
 
 app=Flask(__name__)
 
+@app.route('/')
+def home():
+    app.route('/')
+    return render_template("home.html")
 
 @app.route('/poc/')
 def about():
